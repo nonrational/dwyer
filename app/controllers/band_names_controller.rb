5440ebab9e46ed6d2b535badf634cb45505680d7
Alan Norton <me@alannorton.com>
2014-12-22 17:53:02 -0500

    well that took longer than expected

--- app/controllers/band_names_controller.rb
+++ app/controllers/band_names_controller.rb
@@ -1,12 +1,18 @@
 class BandNamesController < ApplicationController
 
   def index
+    @band_name = BandName.new
+    @band_names = BandName.all.reverse
   end
 
   def create
-    @band_name = BandName.new(params[:band_name])
+    @band_name = BandName.new(post_params).save!
+    redirect_to root_url
+  end
 
-    @band_name.save
-    redirect_to
+  private
+
+  def post_params
+    params.require(:band_name).permit(:name)
   end
 end
