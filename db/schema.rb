3d6b31dd1f4065ecd69579e7034277096625e727
Alan Norton <me@alannorton.com>
2014-12-23 14:20:17 -0600

    show facebook login information on index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223145045) do
+ActiveRecord::Schema.define(version: 20141223201410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141223145045) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "image"
   end
 
 end
