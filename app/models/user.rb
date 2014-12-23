6217b1553b83035a8fbd118c667a7e0a03d97680
Alan Norton <me@alannorton.com>
2014-12-23 14:05:01 -0600

    fix wonky facebook auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
+
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
@@ -9,4 +10,5 @@ class User < ActiveRecord::Base
       user.save!
     end
   end
+
 end
