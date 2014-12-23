da0c732e94a6bb39e751d68d8bfc7f5b54e76485
Alan Norton <me@alannorton.com>
2014-12-23 15:02:51 -0600

    eh. i don't care if it's nullable

--- db/migrate/20141223202821_add_owner_to_band_name.rb
+++ db/migrate/20141223202821_add_owner_to_band_name.rb
@@ -1,7 +1,6 @@
 class AddOwnerToBandName < ActiveRecord::Migration
   def up
     add_column :band_names, :user_id, :integer
-    change_column_null :band_names, :user_id, false
   end
   def down
     remove_column :band_names, :user_id, :integer
