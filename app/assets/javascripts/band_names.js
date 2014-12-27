bffe4125e4895a51765baaf92937957044c80f23
Alan Norton <me@alannorton.com>
2014-12-27 10:41:07 -0600

    javascript to do some cleanup

--- app/assets/javascripts/band_names.js
+++ app/assets/javascripts/band_names.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function removeSpecialChars(str) {
+    return str.replace(/[^\w\s]/gi, '');
+}
+
+$(document).ready(function(){
+    var $band_name_box = $('input.band_name.string');
+
+    $('input.btn.title_case').click(function(){
+        $band_name_box.val(toTitleCase($band_name_box.val()));
+    });
+
+    $('input.btn.remove_special_chars').click(function(){
+        $band_name_box.val(removeSpecialChars($band_name_box.val()));
+    });
+
+});
