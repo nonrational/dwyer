668217ff8c506699091787a12044d516f7a9c431
Alan Norton <me@alannorton.com>
2019-07-30 12:45:19 -0400

    that doesn't work

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,9 +261,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :google_oauth2, ENV.fetch("GOOGLE_CLIENT_ID")
-                                , ENV.fetch("GOOGLE_CLIENT_SECRET")
-                                , skip_jwt: true
+  config.omniauth :google_oauth2, ENV.fetch("GOOGLE_CLIENT_ID"), ENV.fetch("GOOGLE_CLIENT_SECRET"), skip_jwt: true
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
