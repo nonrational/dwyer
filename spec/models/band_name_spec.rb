f3795c217ed9c0e5aa854b7256ae9441ba336cf5
Alan Norton <me@alannorton.com>
2019-12-09 21:37:37 -0500

    validation error for user can't be blank

--- spec/models/band_name_spec.rb
+++ spec/models/band_name_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe BandName do
     subject { BandName.new }
 
     it { expect(subject).not_to be_valid }
+    it do
+      expect { subject.validate! }.to raise_error %r{User can't be blank, Name can't be blank}
+    end
   end
 
   describe '#owner' do
